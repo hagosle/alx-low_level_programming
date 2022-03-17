c10e133f6ad36165e59011af86bf0e04d0cc2072
hagosle <hagosleshifare@gmail.com>
2022-03-18 00:04:44 +0530

    commiting code that checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,5 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
+#include<main.h>
+**
  * main - check the code.
  *
  * Return: Always 0.
@@ -15,40 +13,13 @@ int main(void)
     c = 'a';
     printf("%c: %d\n", c, _isupper(c));
     return (0);
+}[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[B[D[D/**[B[C[C[_isupper -check if aphabet is uppercase[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D@c: print int[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D.[B[D[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dint _isupper(iif[D[C[C[ if (c>= 'A' && c<= 'Z')
+{
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+[A[A[C[[C[C[C[C[C[C[C[C[return (1);
+[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C}
+[C[C[C[C[C[C[C[C[C[else[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Creturn (0);[A[D[D[D[D[D[D[D
+[C[C[C[C[C[C[C{
+[C[C[C[C[C[C[C[[3[C[C[C[C[C[C[C[C[C[C[C[Creturn (0);[A[A[A[A[C[C[D[D[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[Creturn (0);[A[A[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[if (c>= 'A' && c<= 'Z')
+[B[B[B[B[B[B[B[C
+}
