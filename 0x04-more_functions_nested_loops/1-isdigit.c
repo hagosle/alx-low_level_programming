4dad74b065ffeff4f7e06d087f9e58611809a080
hagosle <hagosleshifare@gmail.com>
2022-03-18 01:34:46 +0530

    checkes for digits 0 to9 commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
+}[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[B[B[B[C[C[C[C[C[C[C[C[Cint _isdi[B[B[B[A[A[A[A[A[A[A[A@c: print int[A[A[B[D[D[D[D[D[D[D[_isdigit checks for  a digit.[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B
