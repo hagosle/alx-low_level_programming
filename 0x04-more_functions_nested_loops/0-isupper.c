9176ef830c2978dc820fe7ed0ce5331e1d6c39c9
hagosle <hagosleshifare@gmail.com>
2022-03-18 00:50:37 +0530

    checks for uppercase commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+clear
+[A[C[C[C#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{[A[A[A[A[A[C[C[C[C[_isupper -checks if the letter is uppercase[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D@c: print _isupper(int c)[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+    if(c>= 'A' && c<='Z')
+    {
+     return (1);
+    }
+    else
+    {
+     return (0);
+    }
+}[B
