e08f558a73414d7f828f38bba699adfe0575baf5
hagosle <hagosleshifare@gmail.com>
2022-03-21 23:50:58 +0530

    98 Battery st

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+
 #ifndef _MAIN_H_
 #define _MAIN_H_
 void reset_to_98(int *n);
@@ -11,5 +12,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+int _atoi(char *s);
+
 
 #endif
