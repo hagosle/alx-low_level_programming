5a062f79ecf7593a7315e458f55ce92ebe78ec58
hagosle <hagosleshifare@gmail.com>
2022-03-21 23:58:20 +0530

    98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,14 @@
+
 #include "main.h"
 
 /**
  * reset_to_98 - function that takes a pointer to an int as parameter and updates the value .
  *
- *@c: prints int 
+ *@n: integer pointer. 
  *
  * Return: Always 0.
  */
-int reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 
     n = 402;
