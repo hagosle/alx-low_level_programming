25e0a2c569f1ffebb3a22eec2922f5d06d20a15a
hagosle <hagosleshifare@gmail.com>
2022-03-22 01:22:28 +0530

    generates random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,14 +15,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int count = 0;
 
-	while (i >= 0)
+	while (count >= 0)
 	{
-		*(dest + i) = *(src + i);
-		if (*(src + i) == '\0')
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
 			break;
-		i++;
+		count++;
 	}
 	return (dest);
 }
+
